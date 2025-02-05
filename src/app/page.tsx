91ff0a9aaaba61a784a4fec03890d54f19340205
choace0427 <jacobkevin0427@gmail.com>
2025-02-05 10:51:43 -0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { useAuthStore } from "@/store/authStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Page() {
-	return <>1</>;
+	const user = useAuthStore((state: any) => state.user);
+	const router = useRouter();
+
+	useEffect(() => {
+		if (!user) router.push("/login");
+	}, []);
+
+	return <>coming soon</>;
 }
